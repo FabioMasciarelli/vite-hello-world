3dad7a23e98ec81007b8a254d76a2d77c83d94ce
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-30 14:11:36 +0200

    exsercise completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
+  import AppTitle from './components/AppTitle.vue'
 
+  export default {
+    components: {
+      AppTitle
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
 </script>
 
 <template>
-  
+
+  <div>
+    <AppTitle/>
+
+  </div>
+
+
 </template>
 
 <style>
-
+  div {
+    background-color: blue;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'Simone Bonvecchio'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ title }} </h1>
+</template>
+
+<style>
+    h1 {    
+        color: red;
+    }
+</style>
